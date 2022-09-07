f3ea79eb0d0af711d4a8eda1bb346f0a1a785e17
davidullo <davbarbi@gmail.com>
2022-09-07 18:22:38 +0200

    exercise made for a company

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, EventEmitter } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  constructor(public http: HttpClient) {}
+
+  ngOnInit() {
+    this.loadAllComments();
+  }
+
   title = 'SearchPage';
+  APIURL = 'https://jsonplaceholder.typicode.com/comments?q=';
+  enteredValue: string = '';
+  // searchTextChanged: EventEmitter<string> = new EventEmitter<string>();
+  comments: { id: number; name: string; email: string; body: string }[] = [];
+
+  loadAllComments() {
+    this.http.get(this.APIURL).subscribe((data) => {
+      console.log(data);
+      this.comments = data as {
+        id: number;
+        name: string;
+        email: string;
+        body: string;
+      }[];
+    });
+  }
+
+  loadComments() {
+    this.http.get(this.APIURL + this.enteredValue).subscribe((data) => {
+      console.log(data);
+      this.comments = data as {
+        id: number;
+        name: string;
+        email: string;
+        body: string;
+      }[];
+    });
+  }
+
+  logChars() {
+    console.log(this.enteredValue);
+    console.log(this.enteredValue.length);
+  }
 }
